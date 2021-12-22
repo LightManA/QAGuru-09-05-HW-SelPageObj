ecf043cb2433a865f761aa538104ce2a990c4b97
LightManA <AwarenessLightA@gmail.com>
2021-12-22 15:24:05 +0600

    Added generation of random values for some data (java faker)

--- src/test/java/com/github/LightManA/Tests/GeneralElements.java
+++ src/test/java/com/github/LightManA/Tests/GeneralElements.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.github.LightManA.Components.DateOfBirthFieldAndCalendar;
 import com.github.LightManA.Components.FormWithSubmittedData;
 import com.github.LightManA.Pages.PracticeFormPage;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 
 public class GeneralElements {
@@ -12,6 +13,7 @@ public class GeneralElements {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
     DateOfBirthFieldAndCalendar dateOfBirthFieldAndCalendar = new DateOfBirthFieldAndCalendar();
     FormWithSubmittedData formWithSubmittedData = new FormWithSubmittedData();
+    //Faker faker = new Faker();
 
     @BeforeAll
     static void setMaxBrowserSize() {
--- src/test/java/com/github/LightManA/Tests/TestData.java
+++ src/test/java/com/github/LightManA/Tests/TestData.java
@@ -1,17 +1,22 @@
 package com.github.LightManA.Tests;
 
+import com.github.javafaker.Faker;
+
 public class TestData {
+
+    static Faker faker = new Faker();
+
     static String demoqaURL = "https://demoqa.com/automation-practice-form";
-    static String firstName = "Picaboo";
-    static String lastName = "Pincertone";
-    static String email = "Picaboo@boo.com";
-    static String phoneNumber = "7777318585";
-    static String yearOfBirth = "1985";
-    static String monthOfBirth = "December";
-    static String dayOfBirth = "19";
+    static String firstName = faker.name().firstName();
+    static String lastName = faker.name().lastName();
+    static String email = faker.bothify("????##@gmail.com");
+    static String phoneNumber = faker.regexify("77[0-9]{8}");
+    static String yearOfBirth = faker.regexify("198[0-9]{1}|199[0-9]{1}|200[0-3]{1}");
+    static String monthOfBirth = faker.regexify("November|December|January|February");
+    static String dayOfBirth = faker.regexify("0[1-9]{1}|1[0-9]{1}|2[0-8]{1}");
     static String education = "Computer Science";
     static String nameOfFileWithPhoto = "Leopard.jpg";
-    static String addressHouse = "Chandramurti st., 50, ap.77";
+    static String addressHouse = faker.address().streetAddress();
     static String addressState = "Uttar Pradesh";
     static String addressCity = "Agra";
     static String gender = "Male";
